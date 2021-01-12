4bc8e1b01b49810b195304a1a2d262dc2a295a43
Brandon <winnbrandon2@gmail.com>
2021-01-12 11:07:45 -0800

    BW: Testing resume alt for mobile

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -164,6 +164,13 @@ class NavBar extends React.Component {
       document.getElementById("resume").style.width="80vw";
       document.getElementById("resume").style.height="106.8vw";
       document.getElementById("resume").data = document.getElementById("resume").data;
+      document.getElementById("resume-contents").style.left="10vw";
+      document.getElementById("resume-contents").style.width="80vw";
+      document.getElementById("resume-contents").style.fontSize="1.625vw";
+      document.getElementsByClassName("name").style.fontSize="3.75vw";
+      count = document.getElementsByTagName("b").length;
+      for (var i=0; i<count; i++)
+        document.getElementsByTagName("b")[i].style.fontSizw="1.875vw";
       // Adjust footer content
       document.getElementById("footer").style.height="20vw";
       // Adjust navbar content
@@ -357,6 +364,13 @@ class NavBar extends React.Component {
       document.getElementById("resume").style.width="33.3vw";
       document.getElementById("resume").style.height="44.5vw";
       document.getElementById("resume").data = document.getElementById("resume").data;
+      document.getElementById("resume-contents").style.left="30vw";
+      document.getElementById("resume-contents").style.width="35vw";
+      document.getElementById("resume-contents").style.fontSize="0.65vw";
+      document.getElementsByClassName("name").style.fontSize="1.5vw";
+      count = document.getElementsByTagName("b").length;
+      for (var i=0; i<count; i++)
+        document.getElementsByTagName("b")[i].style.fontSizw="0.75vw";
       // Adjust footer content
       document.getElementById("footer").style.height="8.5vw";
       // Adjust navbar content
