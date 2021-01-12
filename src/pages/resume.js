099ef2a34727a12e129e79efe50128ad0918862b
Brandon <winnbrandon2@gmail.com>
2021-01-12 11:16:04 -0800

    BW: Testing resume alt for mobile 3

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -37,6 +37,7 @@ class Home extends React.Component {
                 <div><b>Organization:</b> Association for Computing Machinery, Title: Logistics Officer, 8/2018 - 5/2019</div>
                 <div>Helped coordinate Beach Hacks 2019 CSULB hackathon. Learned organizational skills, problem solving, and time management.</div> 
             </div>
+            <a id="resume-download" href={Resume} style={{fontSize: "2vw"}} download>Download Resume</a>
           </object>
         </div>
     </div>
